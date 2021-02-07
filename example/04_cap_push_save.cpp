9ff9e74dc78f5e500c17888479287c5288971b9f
weishao <xweimvp@gmail.com>
2021-02-07 17:17:29 +0800

    换成官方api检测device

--- example/04_cap_push_save.cpp
+++ example/04_cap_push_save.cpp
@@ -4,37 +4,91 @@
 #include "rtspthread.h"
 #include "AACEncoderT.h"
 
-
+#include <stdio.h>
 char device[2048] = {0};
-const char* device_format = "plughw:%c,%c";
+const char* device_format = "plughw:%d,%d";
 
-void checkDevice(){
-    char device_num[2];
 
-    FILE* fp;
-    char pBuffer[128];
-    fp = popen("arecord -l |grep card | awk  '{print $2}'","r"); 
-    fgets(pBuffer,sizeof(pBuffer),fp);
-    device_num[0] = pBuffer[0]; //card
-    std::cout<<device_num[0]<<endl;
 
-    memset(pBuffer,0,sizeof(pBuffer));
-    fp = popen("arecord -l |grep device | awk  '{print $6}'","r"); 
-    fgets(pBuffer,sizeof(pBuffer),fp);
-    device_num[1] = pBuffer[0]; //device
-    std::cout<<device_num[1]<<endl;
+static void cap_device_list(void)
+{
+    snd_pcm_stream_t stream = SND_PCM_STREAM_CAPTURE;
+	snd_ctl_t *handle;
+	int card, err, dev, idx;
+	snd_ctl_card_info_t *info;
+	snd_pcm_info_t *pcminfo;
+	snd_ctl_card_info_alloca(&info);
+	snd_pcm_info_alloca(&pcminfo);
 
-    snprintf(device,sizeof(device),device_format,device_num[0],device_num[1]);
-    std::cout<<device<<endl;
-    fclose(fp);
-
+	card = -1;
+	if (snd_card_next(&card) < 0 || card < 0) {
+		printf("no soundcards found...");
+		return;
+	}
+	printf("**** List of %s Hardware Devices ****\n",
+	       snd_pcm_stream_name(stream));
+	while (card >= 0) {
+		char name[32];
+		sprintf(name, "hw:%d", card);
+		if ((err = snd_ctl_open(&handle, name, 0)) < 0) {
+			printf("control open (%i): %s", card, snd_strerror(err));
+			goto next_card;
+		}
+		if ((err = snd_ctl_card_info(handle, info)) < 0) {
+			printf("control hardware info (%i): %s", card, snd_strerror(err));
+			snd_ctl_close(handle);
+			goto next_card;
+		}
+		dev = -1;
+		while (1) {
+			unsigned int count;
+			if (snd_ctl_pcm_next_device(handle, &dev)<0)
+				printf("snd_ctl_pcm_next_device");
+			if (dev < 0)
+				break;
+			snd_pcm_info_set_device(pcminfo, dev);
+			snd_pcm_info_set_subdevice(pcminfo, 0);
+			snd_pcm_info_set_stream(pcminfo, stream);
+			if ((err = snd_ctl_pcm_info(handle, pcminfo)) < 0) {
+				if (err != -ENOENT)
+					printf("control digital audio info (%i): %s", card, snd_strerror(err));
+				continue;
+			}
+            snprintf(device,sizeof(device),device_format,card,dev);
+            std::cout<<device<<endl;
+			printf("card %i: %s [%s], device %i: %s [%s]\n",
+				card, snd_ctl_card_info_get_id(info), snd_ctl_card_info_get_name(info),
+				dev,
+				snd_pcm_info_get_id(pcminfo),
+				snd_pcm_info_get_name(pcminfo));
+			count = snd_pcm_info_get_subdevices_count(pcminfo);
+			printf("  Subdevices: %i/%i\n",snd_pcm_info_get_subdevices_avail(pcminfo), count);
+			for (idx = 0; idx < (int)count; idx++) {
+				snd_pcm_info_set_subdevice(pcminfo, idx);
+				if ((err = snd_ctl_pcm_info(handle, pcminfo)) < 0) {
+					printf("control digital audio playback info (%i): %s", card, snd_strerror(err));
+				} else {
+					printf("  Subdevice #%i: %s\n",
+						idx, snd_pcm_info_get_subdevice_name(pcminfo));
+				}
+			}
+		}
+		snd_ctl_close(handle);
+	next_card:
+		if (snd_card_next(&card) < 0) {
+			printf("snd_card_next");
+			break;
+		}
+	}
 
 }
+
 
 int main(int argc, char *argv[])
 {
 
-    checkDevice();
+    cap_device_list();
+
     //设置需要采集pcm参数
     unsigned long   nSampleRate = 32000;
     unsigned int    nChannels	= 2;
